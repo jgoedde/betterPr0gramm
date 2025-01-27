139c536b230a10462564f2359cbcfe8749e2b406
Julian Gödde <goedde@tuta.io>
2025-01-27 19:39:16 +0100

    Make video sound opt-in instead of opt-out

--- src/contexts/playback/PlaybackProvider.tsx
+++ src/contexts/playback/PlaybackProvider.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode, useMemo, useState } from "react";
 import { PlaybackContext } from "./PlaybackContext";
 
 export const PlaybackProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const [shouldPlayAudio, setShouldPlayAudio] = useState(true);
+    const [shouldPlayAudio, setShouldPlayAudio] = useState(false); // Idea: We could store that in LS.
     const [shouldShowCaptions] = useState(false);
 
     const value = useMemo(() => {
