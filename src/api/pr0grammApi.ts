39963ccb330a50952a62afa6079c5cc69c3d845c
Julian Gödde <goedde@tuta.io>
2025-03-15 18:14:44 +0100

    feat: Update API base URL to use VITE_PROXY_URL in pr0grammApi.ts and add .env.example with VITE_PROXY_URL
    Resolves #19

--- src/api/pr0grammApi.ts
+++ src/api/pr0grammApi.ts
@@ -1,2 +1,1 @@
-// export const BASE_URL = "http://localhost:3000"; // TODO: Make this to a .env
-export const BASE_URL = "https://better-pr0gramm-proxy.vercel.app"; // TODO: Make this to a .env
+export const BASE_URL = import.meta.env.VITE_PROXY_URL;
